8541c6f0652d6481350c0e75cf276cf42722ede7
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2026-03-09 09:22:28 +0700

    Fix : login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -175,7 +175,7 @@ export default function Example() {
               src={LOGO}
               alt="Your Company"
             />
-            <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-white">
               Masuk ke akun Anda
             </h2>
           </div>
